87d12eabd4dd8947610b34a7fe02faec18782563
Abdul Rehaman Sk <134486438+AbdulRehaman-Shaik@users.noreply.github.com>
2025-09-20 11:58:57 +0530

    Create question10.sv

---
+++ Assignment3/question10.sv
@@ -0,0 +1,33 @@
+//Exercise-10
+
+class trans;
+	int i;
+
+	function new();	//constructor
+		i = 10;
+	endfunction
+endclass : trans
+
+	class ex_trans1 extends trans;
+		int i;
+
+		function new(int i);	//constructor
+			this.i = i;
+		endfunction
+	endclass : ex_trans1
+
+	ex_trans1 e_h;
+
+	module test();
+		initial
+			begin
+				e_h = new(10);
+				$display("%p",e_h);
+			end
+	endmodule
+
+/* output 
+
+	'{i:10, i:10}
+
+*/
